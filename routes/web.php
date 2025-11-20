29b2bd227a1243983d4396a3262bf2fa954efb3c
bosiiipo <olabosiposogbolu@gmail.com>
2025-11-20 07:56:58 +0100

    seeder

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-seeder', function() {
+    if (config('app.env') === 'production') {
+        abort(403, 'Forbidden');
+    }
+
+    $userId = DB::table('users')->insertGetId([
+        'name' => 'John Did',
+        'email' => 'john@google.com',
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]);
+
+    DB::table('wallets')->insert([
+        'user_id' => $userId,
+        'currency' => 'NGN',
+        'balance' => 100000.00,
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]);
+    return 'Seeder executed!';
+});
